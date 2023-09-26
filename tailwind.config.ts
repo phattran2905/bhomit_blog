aab91978d13edb6e3f3c41e9e26e5862a3e3b602
phat@minim1 <phat.tranducvn@gmail.com>
2023-09-26 16:21:44 -0700

    Add theme colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				heading: "Raleway",
+				body: "Roboto",
+			},
+			colors: {
+				primary: "#3D3474",
+				"primary-80": "#584E8D",
+				"primary-60": "#8C84B7",
+				"primary-40": "#409DF9",
+				"primary-20": "#92C9FF",
+				"primary-light": "#DBEDFF",
+				"title-color": "#333333",
+				"body-color": "#666666",
+				"in-field-color": "#999999",
+				"disable-color": "#CCCCCC",
+				white: "#ffffff",
+				fill: "#8624A8",
+				"light-blue": "#F7F7FC",
+				orange: "#FF6715",
+				"light-green": "#43CC74",
+				"accent-red": "#F7473E",
+				"image-placeholder": "#EEEEEE",
+			},
+			backgroundImage: {
+				heroBg: "url('/assets/img/bg/hero-bg.png')",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
