dbe8eafdb075aef9ed777d82993f5a805eb3945d
phat@minim1 <phat.tranducvn@gmail.com>
2023-10-08 20:14:38 -0700

    Fix session on client side

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { getServerSession } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-async function AuthProvider({ children }: { children: React.ReactNode }) {
-	const session = await getServerSession();
-	return <SessionProvider session={session}>{children}</SessionProvider>;
+function AuthProvider({ children }: { children: React.ReactNode }) {
+	return <SessionProvider>{children}</SessionProvider>;
 }
 export default AuthProvider;
